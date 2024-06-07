18d91a84e4e071b6555b00a3c562c946a3297bd1
DESKTOP-8H1UBMN\Mark <markbtesh@gmail.com>
2024-06-07 17:32:49 -0400

    adjusted view for mobile and fonts

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto overflow-hidden">
 
-      <div className={`${styles.paddingX} absolute inset-0 sm:top-[350px] xs:top-[350px] top-[75px] max-w-7x1 mx-auto flex flex-row items-start gap-5`}>
+      <div className={`${styles.paddingX} absolute inset-0 sm:top-[350px] xs:top-[350px] lg:top-[75px] max-w-7x1 mx-auto flex flex-row items-start gap-5`}>
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#FF5D29]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
