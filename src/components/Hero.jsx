32cd417c68d468417a91034d8ffbb97324f304f5
DESKTOP-8H1UBMN\Mark <markbtesh@gmail.com>
2024-06-07 17:21:21 -0400

    adjusted view for mobile and fonts

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto overflow-hidden">
 
-      <div className={`${styles.paddingX} absolute inset-0 sm:top-[120px] top-[75px] max-w-7x1 mx-auto flex flex-row items-start gap-5`}>
+      <div className={`${styles.paddingX} absolute inset-0 sm:top-[120px] xs:top-[350px] top-[75px] max-w-7x1 mx-auto flex flex-row items-start gap-5`}>
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#FF5D29]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
@@ -22,7 +22,7 @@ const Hero = () => {
         </div>
 
         <div className='z-20 absolute sm:left-[100px] left-[50px] '>
-        <h1 className={`${styles.heroHeadText} heroBubbleText`}> Tres <br /> Comma <br /><span className="text-[#FF5D29] heroBubbleText"> Club</span></h1>
+        <h1 className={`${styles.heroHeadText} heroBubbleText`}> tReS <br /> ComMA  <br /><span className="text-[#FF5D29] heroBubbleText"> CLuB</span></h1>
         <p className={`${styles.heroSubText} mt=2 text-white-100`}>
            <br className="sm:block hidden" /> 
         </p>
@@ -30,7 +30,7 @@ const Hero = () => {
        
         <br /> 
         <div>
-          <a href="" className='rounded-full bg-[#FF5D29] p-5 px-10'> BUY NOW</a>
+          <a href="" className='rounded-full bg-[#FF5D29] p-5 px-10 text-[24px]'> BUy NoW</a>
         </div>
         <br />
         
@@ -38,7 +38,7 @@ const Hero = () => {
       </div>
       <div className='absolute bottom-20'>
       <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
-        <FontAwesomeIcon icon={faTwitter} color='#1877F2' size="4x" className="pr-3"/>
+        <FontAwesomeIcon icon={faTwitter} color='#1877F2' size="5x" className="pr-3"/>
       </a>
       
    
@@ -48,7 +48,7 @@ const Hero = () => {
 
    
       
-      <div className='absolute left-[14vw] w-full flex justify-center items-center'>
+      <div className='absolute lg:left-[14vw] xs:top-10 lg:top-0  w-full flex justify-center items-center'>
         
               <div className="slider-thumbnail">
                 <img src={banner} alt="Slider Images" />
@@ -59,10 +59,10 @@ const Hero = () => {
 
         <div className='absolute right-32  top-10 w-full flex justify-center items-center'>
         
-        <div className="chat-bubble">
-        <img src={bubble} />
-        <h2 className={`${styles.dollar} absolute  left-[42.6vw] inline-block top-[-4px]`}>$</h2>
-        <h2 className={`${styles.heroBubbleText} absolute left-[45vw] top-0 `}> CLUB</h2>
+        <div >
+        <img className="chat-bubble" src={bubble} />
+        <h2 className={`${styles.dollar} absolute  left-[42.6vw] inline-block top-[-4px]`}></h2>
+        <h2 className={`${styles.heroBubbleText} absolute lg:left-[45vw] xs:left-[46vw] xs:top-[0px] lg:top-10 `}> $CLUB</h2>
         </div>
        
 
